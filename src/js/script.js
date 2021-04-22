1ad57107b02447d91c7607681742cfe3bbbada01
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-22 19:50:35 +0200

    fix initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -115,10 +115,11 @@
         /*prevent default action for event */
         event.preventDefault();
         /*find active product (product that has active class)*/
-        const activeProduct = thisProduct.element.querySelector('????????');
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
         console.log('activeProduct:', activeProduct);
+        console.log('this:', thisProduct.element);
         /*if there is active product and it's not thisProduct.element, remove class active from it */
-        if (thisProduct.element == activeProduct){
+        if (activeProduct != null && activeProduct != thisProduct.element){
           activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
