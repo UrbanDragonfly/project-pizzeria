027d667548f7211bec388e4d8909dd95c41d92c9
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-23 15:50:23 +0200

    add processOrder method code

--- dist/js/script.js
+++ dist/js/script.js
@@ -115,11 +115,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
-      console.log('>acordionTrigger>:', thisProduct.accordionTrigger);
-      console.log('>form>>>>>>>>>>>>:', thisProduct.form);
-      console.log('>formInputs>>>>>>:', thisProduct.formInputs);
-      console.log('>cartButton>>>>>>:', thisProduct.cartButton);
-      console.log('>priceElem>>>>>>>:', thisProduct.priceElem);
+
     }
 
     initAccordion(){
@@ -145,12 +141,66 @@
 
     initOrderForm(){
       const thisProduct = this;
-      console.log('thisProduct initOrderForm:', thisProduct);
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
     }
 
     processOrder(){
       const thisProduct = this;
-      console.log('thisProduct processOrder:', thisProduct);
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+
+        // for every option in this category
+        for(let optionId in param.options) {
+
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+
+            // check if the option is not default
+            if(!option.default === true) {
+
+              // add option price to price variable
+              price += option.price;
+            }
+          } else {
+
+            // check if the option is default
+            if(option.default === true) {
+
+              // reduce price variable
+              price -= option.price;
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
 
   }
